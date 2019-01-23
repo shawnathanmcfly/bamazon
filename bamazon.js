2ab0389cd3750d52d86b8da33e38e6bc4b5bd413
shawnathanmcfly <sachimasi9187@gmail.com>
2019-01-23 11:19:24 -0500

    first commit

---
+++ bamazon.js
@@ -0,0 +1,314 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+const cTable = require('console.table');
+var basket = [];
+
+//Takes in returned mySQL data and formats it neatly for an inquirer list
+function formatArrayOfObjects( arr ){
+
+    let t = [], largest, formatted = [];
+
+    //get column names to beginning of array
+    t.push( Object.keys( arr[0] ));
+    
+    //convert objects values to array
+    for( let i = 0; i < arr.length; i++ ){
+        t.push( Object.values( arr[i] ));
+    }
+
+    for( let i = 0; i < t[0].length; i++ ){
+
+        largest = 0;
+
+        //find largest string in table
+        for( let ii = 0; ii < t.length; ii++ ){
+
+            if( typeof(t[ii][i]) != "string" )
+                t[ii][i] = String( t[ii][i] );
+
+            if( t[ii][i].length > largest)
+                largest = t[ii][i].length;
+        }
+        
+        for( let ii = 0; ii < t.length; ii++ ){
+
+            let padding = largest - t[ii][i].length;
+            while( padding-- )
+                t[ii][i] += " ";    
+        }
+    }
+
+    for( let i = 0; i < t.length; i++ ){
+        formatted.push( "" );
+
+        for( let ii = 0; ii < t[i].length; ii++ )
+            formatted[i] += t[i][ii] + "  ";
+    }
+
+    return {
+
+        header: formatted.slice( 0, 1 ),
+        list: formatted.slice( 1 )
+
+    }
+}
+
+function insertItem( item ){
+    var sql = "INSERT INTO products (product_name, department, price, stock) \
+        VALUES ('" + item.name + "', '" + item.dept + "', " + item.price + ", " + item.amount + ")";
+    con.query(sql, function (err, result) {
+        if (err)
+            console.log( "ERROR:" + err );
+        else{
+            con.query("SELECT * FROM products", (err, result) => {
+                if (err) throw err;
+                console.log(result);
+            });
+        }
+  });
+}
+
+function manager(){
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: 'mp',                                      
+            message: "Welcome, manager. How can I assist you today?",
+            choices: [
+                "View Products For Sale",
+                "View Low Inventory",
+                "Add To Inventory",
+                "Add New Product",
+                "Quit"
+            ]
+        }
+    ]).then( answer => {
+
+        switch( answer.mp ){
+            case "Quit":
+                console.log( "Goodbye Manager, please turn off the lights and deposit store tills at bank :)");
+                con.end();
+                break;
+            case "View Products For Sale":
+                con.query("SELECT * FROM products", (err, result) => {
+                    if( err ) throw err;
+                    console.log("");
+                    console.table( result );
+                    manager();
+                });
+                break;
+            case "View Low Inventory":
+                con.query("SELECT * FROM products WHERE stock < 5", (err, result) => {
+                    if( err ) throw err;
+                    console.log("");
+                    console.log("******************");
+                    console.log("* Low Stock View *");
+                    console.log("******************");
+                    console.table( result );
+                    manager();
+                    
+                });
+                break;
+            case "Add To Inventory":
+                con.query("SELECT * FROM products", (err, result) => {
+                    if( err ) throw err;
+                    let sqlQ = formatArrayOfObjects( result );
+
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'inv',
+                            message: sqlQ.header,
+                            choices: sqlQ.list,
+                            filter: v => {
+
+                                return parseFloat( v.charAt(0) );
+                            }
+                        }
+                    ]).then( answer => {
+
+                        inquirer.prompt([
+                            {
+                                type: 'input',
+                                name: 'stock',
+                                message: 'How many do you want to add to inventory?',
+                                
+                            }
+                        ]).then( amt => {
+                            con.query("UPDATE products SET stock = stock + " + amt.stock + 
+                                " WHERE item_id = " + answer.inv, err => {
+                                if( err ) throw error; else console.log( "Stock updated!" )
+                                manager();
+                            });
+                        });   
+                    });
+                })
+                
+                break;
+            case "Add New Product":
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'Please enter title of game'
+                    },
+                    {
+                        type: 'input',
+                        name: 'dept',
+                        message: 'Please enter the platform the game id for'
+                    },
+                    {
+                        type: 'input',
+                        name: 'price',
+                        message: 'What is the price?'
+                    },
+                    {
+                        type: 'input',
+                        name: 'amount',
+                        message: 'How many?',
+                        default: '1'
+                    }
+                ]).then( a => {
+                    insertItem( a );
+                    manager();
+                });
+                break;
+        }
+
+    });
+}
+
+function shop(){
+    con.query("SELECT * FROM products", (err, result, fields) => {
+        if (err) throw err;
+        
+        var itemList = [];
+
+        //adjust game selection based on what's in buyers basket
+        for( let i = 0; i < result.length; i++ ){
+
+            for( let ii = 0; ii < basket.length; ii++)
+                if( basket[ii].item === result[i].product_name )
+                    result[i].stock -= basket[ii].amount;
+                        
+            if( result[i].stock )
+                itemList.push( result[i].product_name + "   $" + result[i].price + "   In Stock: " + result[i].stock );        
+        }
+
+        //add option to stop shopping
+        itemList.push( "Leave" );
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'shop',
+                message: 'Check these retro games out, bro!?\n',
+                choices: itemList,
+                filter: v => {
+
+                    if( v != "Leave" ){
+                        let t = v.replace(/ /g, ""), found = false;
+                        t = t.split("$");
+                        
+                        //search if buyer already has item in basket. If so, add it to amount of item in basket
+                        for( let i = 0; i < basket.length; i++ ){
+
+                            if( basket[i].item === t[0] ){
+                                console.log("here's another one!");
+                                basket[i].amount++;
+                                found = true;
+                            }
+                        }
+
+                        if( !found ){
+                            basket.push({
+
+                                item: t[0],
+                                price: parseFloat( t[1] ),
+                                amount: 1
+
+                            });
+                        }
+                    }
+                }
+            },{
+
+                type: 'confirm',
+                name: 'buyMore',
+                message: 'Would you like anything else?',
+                default: true
+            }
+        ]).then( answers => {
+
+            if( answers.buyMore )
+                shop();
+            else{
+
+                if( basket.length ){
+
+                    let total = 0;
+                    for( let i = 0; i < basket.length; i++ )
+                        total += basket[i].price * basket[i].amount;
+
+                    console.log( " Your total is: " + total );
+
+                    //last mysql database adjust
+                    for( let i = 0; i < result.length; i++ )
+                        for( let ii = 0; ii < basket.length; ii++)
+                            if( basket[ii].item === result[i].product_name )
+                                result[i].stock -= basket[ii].amount;     
+                    
+                    for( let i = 0; i < result.length; i++ ){
+                        con.query( "UPDATE products SET stock = '" + result[i].stock  + 
+                            "' WHERE product_name = '" + result[i].product_name +"'", err  => {
+                            if( err) throw err;
+                        });
+                    }
+                }
+                    
+                console.log( " Thanks for stopping by, bro!" );
+
+            }
+        });
+    });
+}
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "alu1ndra1",
+    database: "bamazon"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+
+    switch( process.argv[2] ){
+
+        case "manager":
+
+            manager();
+            
+            break;
+        case "shop":
+
+            shop();
+            
+            break;
+        case "-help":
+
+        case 
+
+            //help menu
+            console.log( "Coming Soon...");
+            
+            break;
+        default:
+            console.log( 'Invalid command, type "node bamazon.js -help" for help' );
+            con.end();
+            break;
+    }
+
+
+});
